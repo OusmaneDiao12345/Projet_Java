b6f0493e766f78c1caf7cde28b489c8444815065
OusmaneDiao12345 <159960934+OusmaneDiao12345@users.noreply.github.com>
2024-12-21 13:41:23 +0100

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class DatabaseConnection {
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/projet_file_rouge";
-    private static final String USER = "root"; // Remplacez par votre nom d'utilisateur
-    private static final String PASS = ""; // Remplacez par votre mot de passe
+    private static final String USER = "root"; 
+    private static final String PASS = ""; 
 
     public static Connection getConnection() {
         Connection conn = null;
@@ -16,7 +16,7 @@ public class DatabaseConnection {
             return conn;
         } catch (SQLException e) {
             System.err.println("Erreur de connexion :");
-            e.printStackTrace(); // Importe pour afficher le détail de l'erreur
+            e.printStackTrace(); 
             return null;
         }
     }
